67bdc5ea0ba62838298a55e4e08091f76cdd8ca7
Vahid Sayad <vahidsayad@gmail.com>
2022-06-08 13:11:13 +0430

    Initial Commit

---
+++ TEST/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TEST
+//
+//  Created by Vahid Sayad on 8/6/2022 .
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TEST/TESTApp.swift
@@ -0,0 +1,17 @@
+//
+//  TESTApp.swift
+//  TEST
+//
+//  Created by Vahid Sayad on 8/6/2022 .
+//
+
+import SwiftUI
+
+@main
+struct TESTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
